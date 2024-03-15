e310a89738668856bfc8f003dc0d0e209268596b
Shubham Kumar <ishubhamjnv@gmail.com>
2024-03-15 15:52:27 +0530

    add favicon

--- src/components/YoutubeChannel/YoutubeChannel.jsx
+++ src/components/YoutubeChannel/YoutubeChannel.jsx
@@ -1,6 +1,5 @@
 // YouTubeChannel.jsx
 import React from "react";
-import { Link } from "react-router-dom";
 import "./YoutubeChannel.css";
 import youtubeLogoImg from "../../assets/images/youtube.png";
 
